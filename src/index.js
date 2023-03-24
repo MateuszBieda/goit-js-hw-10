7f07ce08596f81d55718223d998b1185dfa2d3b8
MateuszBieda <mateusbieda@gmail.com>
2023-03-25 00:55:03 +0100

    "functions update"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const textInput = document.querySelector("#search-box");
+const name=""; 
+const countriesArray = [];
+export {countriesArray};
+// function fetchCountries(name){
+//     return fetch ("https://restcountries.com/v3.1/name/`${name}`")
+//     .then( (response) =>{
+//         if(!response.ok){
+//             throw new Error('Parameter is not a letter!');
+//         }
+//         response.json();
+//     } 
+//     );
+// }
+
+// function fetchCountries(name){
+//     return fetch ("https://restcountries.com/v3.1/name/`${name}`")
+//     .then( (response) =>{
+//         response.json();
+//     } 
+//     )
+//     .then(data => console.log(data));
+// }
+
+
+fetch (`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => response.json())
+    .then(data => console.log(data)) 
+
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(
+        (response) => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+        }
+    )
+    .then(data =>console.log(data))
+}
+
+textInput.addEventListener("input", () => {
+    fetchCountries(name)
+          .then((name) => console.log(name.common))
+          .catch((error) => console.log("Oops, there is no country with that name"));
+      });
